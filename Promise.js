008af11ddecd6532c7c12dbe5f5aae0436e531e6
MaShizhen <31983261+MaShizhen@users.noreply.github.com>
2017-09-28 13:53:36 +0800

    Update Promise.js

--- Promise.js
+++ Promise.js
@@ -1,14 +1,14 @@
 let Myjson = {};
-function a(){
-    var p = new Promise(function(resolve, reject){
+const a =()=>{
+    const p = new Promise(function(resolve, reject){
         setTimeout(function(){
             resolve("sucess");
         }, 2000);
     });
     return p;            
 }
-function b(){
-    var p1 = new Promise(function (resolve, reject) {
+const b = ()=>{
+    const p1 = new Promise(function (resolve, reject) {
             setTimeout(function(){
                  reject("err");
              }, 3000);
